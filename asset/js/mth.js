374645a81b35e03246092fa3a10d117b539b124e
NN17 <nyinyihan25714@gmail.com>
2022-07-16 11:46:01 +0630

    mobile slide

--- asset/js/mth.js
+++ asset/js/mth.js
@@ -3,11 +3,15 @@ function mediaMobile(x) {
 		$("#slide1").attr('src', 'asset/upload_img/rf_mobile.jpg');
 		$("#slide2").attr('src', 'asset/upload_img/solar_mobile.jpg');
 		$("#slide3").attr('src', 'asset/upload_img/enc_mobile.jpg');
+		$("#slide4").attr('src', 'asset/upload_img/mfp_mobile.jpg');
+		$("#slide5").attr('src', 'asset/upload_img/shekinah_mobile.jpg');
 	}
 		else {
 			$("#slide1").attr('src', 'asset/upload_img/rainflower_slide.jpg');
 			$("#slide2").attr('src', 'asset/upload_img/solar_slide.jpg');
 			$("#slide3").attr('src', 'asset/upload_img/enc.jpg');
+			$("#slide4").attr('src', 'asset/upload_img/mfp_slide.jpg');
+			$("#slide5").attr('src', 'asset/upload_img/shekinah_slide.jpg');
 		}
 }
 
